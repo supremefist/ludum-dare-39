6cdae23c0755bbba4c9803ab19d71da28f209e98
supremefist <sakkie99@gmail.com>
2017-07-27 12:14:34 +0200

    Animation

--- js/ld39/entities/pacman-entity.js
+++ js/ld39/entities/pacman-entity.js
@@ -5,12 +5,75 @@ LD39.PacmanEntity = function() {
 
   var texture = PIXI.loader.resources["graphics/pacman"].texture;
 
-  var pacmanTexture = new PIXI.Texture(
-    texture, new PIXI.Rectangle(1, 1, 32, 32));
-  var pacmanSprite = new PIXI.Sprite(pacmanTexture);
+  var pacmanFrameTextures = [];
+  for (var i = 0; i < 2; i++) {
+    pacmanFrameTextures.push(new PIXI.Texture(
+      texture, new PIXI.Rectangle(1 + i * 34, 1, 32, 32)));
+  }
+
+  var pacmanSprite = new PIXI.extras.AnimatedSprite(pacmanFrameTextures);
 
   this.sprite = pacmanSprite;
+  this.sprite.animationSpeed = 0.1;
+  this.sprite.anchor.set(0.5, 0.5);
+
+  this.movementTimer = 0;
+  this.movementAlternator = true;
+  this.alreadyMoving = false;
 }
 
 LD39.PacmanEntity.prototype = Object.create(LD39.Entity.prototype);
 LD39.PacmanEntity.prototype.constructor = LD39.PacmanEntity;
+
+LD39.PacmanEntity.prototype.update = function(delta) {
+  LD39.Entity.prototype.update.call(this, delta);
+  this.interpretMovement(delta);
+}
+
+LD39.PacmanEntity.prototype.interpretMovement = function(delta) {
+  var moving = false;
+  if (this.velocityX > 0) {
+    this.sprite.scale.x = 1.0;
+    this.sprite.rotation = 0;
+    moving = true;
+  } else if (this.velocityX < 0) {
+    this.sprite.scale.x = -1.0;
+    this.sprite.rotation = 0;
+    moving = true;
+  }
+
+  if (this.velocityY > 0) {
+    this.sprite.rotation = Math.PI * 0.5;
+    this.sprite.scale.x = 1.0;
+    moving = true;
+  } else if (this.velocityY < 0) {
+    this.sprite.rotation = Math.PI * 1.5;
+    this.sprite.scale.x = 1.0;
+    moving = true;
+  }
+
+  if (moving) {
+    this.movementTimer += delta;
+    if (!this.alreadyMoving) {
+      this.sprite.play();
+    }
+    this.alreadyMoving = true;
+  } else {
+    if (this.alreadyMoving) {
+      this.sprite.gotoAndStop(0);
+    }
+    this.alreadyMoving = false;
+  }
+
+  if (this.movementTimer > 200) {
+    if (this.movementAlternator) {
+      PIXI.loader.resources["sounds/bump_b"].sound.play();
+      this.movementAlternator = false;
+    } else {
+      PIXI.loader.resources["sounds/bump_c"].sound.play();
+      this.movementAlternator = true;
+    }
+
+    this.movementTimer = 0;
+  }
+}
